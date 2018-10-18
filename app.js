2ab72244d57b565fc6c71498524f7ac33ba656c9
Paula Suarez <pauladzuck@hotmail.com>
2018-10-18 05:42:24 -0500

    Adding updates to app.js

--- app.js
+++ app.js
@@ -6,15 +6,19 @@ $(document).ready(function () {
         //global variables 
         var i = 0;
         var j;
-        // create the h3 element
-        var h3 = $('<h3>Accusation</h3>');
+
 
         for (j = 0; j < 100; j++) {
+            // create the h3 element
+            var h3 = $('<h3>Accusation</h3>');
             //append h3 to body
-            $('body').append(h3);
-  
+            $('body').append(h3)
+            $(h3).append(' ' + j)
+    
+    
+
         }
-        
+
         $(h3).click(function () {
             console.log('true');
         });
