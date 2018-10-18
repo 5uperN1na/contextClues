e5b299ac16c6d95a932c53f5f8c512ec7b6809a4
Paula Suarez <pauladzuck@hotmail.com>
2018-10-18 08:41:27 -0500

    Updates to app.js file

--- app.js
+++ app.js
@@ -7,6 +7,17 @@ $(document).ready(function () {
         var i = 0;
         var j;
 
+        //array that lists friend's name.
+        var friends = ['Erica', 'David', 'Meredith', 'Hernan', 'Ashley'];
+
+
+        //array that lists locations
+        var locations = ['Dining room', 'Kitchen', 'Living room', 'Hallway', 'Master bedroom', 'Master bathroom', 'Guest bedroom', 'Guest bathroom', 'Nursery', 'Basement'];
+
+        //array that lists weapons
+        var weapons = ['Knife', 'Rope', 'Rolling pin', 'Hammer', 'Pencil', 'Stick', 'Spatula', 'Frying pan', 'Flower pot', 'Scissors', 'Chair', 'Candlestick', 'Fork', 'Screw driver', 'Key', 'Lamp', 'Pitch fork', 'Vase', 'Curtain rod', 'Table leg'];
+
+
 
         for (j = 0; j < 100; j++) {
             // create the h3 element
@@ -14,13 +25,18 @@ $(document).ready(function () {
             //append h3 to body
             $('body').append(h3)
             $(h3).append(' ' + j)
-    
-    
-
         }
 
         $(h3).click(function () {
             console.log('true');
+
+
+
+
+
+
+
+
         });
     })
 
